8514d66ff936533eb33b1ddffab8f72e1178e593
Ivan Dyakov <dyakovivan@hotmail.com>
2024-07-18 18:44:20 +0200

    Добавьте файлы проекта.

---
+++ CircleAreaAndCircumference.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CircleAreaAndCircumference
+{
+    internal class CircleAreaAndCircumference
+    {
+        static void Main(string[] args)
+        {
+            int radius = 30; //TODO: Осуществить ввод с консоли
+            double circleArea = Math.PI * Math.Pow(radius, 2);
+            double circumference = 2 * Math.PI * radius;
+
+            Console.WriteLine("Площадь круга равна: {0,20:f4}", circleArea);
+            Console.WriteLine("Длина окружности равна: {0,16:f4}", circumference);
+
+            Console.ReadKey();
+        }
+    }
+}
